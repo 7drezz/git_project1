ab13e964660a3b3327a2f5d8c9dd9f1b28e1ad53
7drezz <fedor06032007@gmail.com>
2023-11-18 19:38:13 +0300

    added function

--- program.py
+++ program.py
@@ -1,9 +1,14 @@
+def my_superfunction():
+    print('It is new line')
+
+
 def main():
     print('My first git program')
     print('And I change it every day')
     print('Again')
     print('UFO came and added this line')
-    print('It is new line')
+    my_superfunction()
+
 
 if __name__ == '__main__':
     main()
